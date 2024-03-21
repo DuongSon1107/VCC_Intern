7439009ec13c4e399f83386d0bf25e639a0ed7ee
Triệu Dương Sơn <98730673+DuongSon1107@users.noreply.github.com>
2024-03-21 09:35:21 +0700

    Delete VCCWeek1/src/Week1_OOP/NhanSuCongTy directory

--- VCCWeek1/src/Week1_OOP/NhanSuCongTy/Info.java
+++
@@ -1,9 +0,0 @@
-package Week1_OOP.NhanSuCongTy;
-
-public interface Info {
-    int getAge();
-    int getDayOnWork();
-    String getBirthday();
-    String getName();
-    String getPosition();
-}
